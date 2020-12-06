42f34d8ac16516958d5ba78a12c5f9203ebe2732
Elyor <e.hadjihanov@gmail.com>
2020-12-06 20:07:20 +0500

    Added vehicles to vehicles.html

--- scripts/script.js
+++ scripts/script.js
@@ -32,32 +32,42 @@
 })();
 
 
-
 // This piece of code pastes header into each element with class header
 // I did this to remove duplication, because navbar(header) is same on each page
-let headers = document.getElementsByClassName("header");
-for (let i = 0; i < headers.length; i++) {
-    headers[i].innerHTML = String.raw`<div class="logo-with-text-container">
-                    <img src="images/logo.svg" alt="Mercedes logo" class="logo">
-                    <img src="images/mercedes-benz-text.svg" alt="Mercedes-Benz" class="mercedes-benz-text">
-                </div>
-                <nav class="navbar">
-                    <ul class="nav-list">
-                        <li class="nav-item"">
-                            <a href="index.html" class="nav-link" id="home-link">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="vehicles.html" class="nav-link">Vehicles</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="design.html" class="nav-link">Design</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="history.html" class="nav-link">History</a>
-                        </li>
-                    </ul>
-                </nav>`;
-}
+(function () {
+    let headers = document.querySelectorAll(".header");
+    headers.forEach(header => {
+        header.innerHTML = String.raw`<div class="logo-with-text-container">
+        <img src="images/logo.svg" alt="Mercedes logo" class="logo">
+        <img src="images/mercedes-benz-text.svg" alt="Mercedes-Benz" class="mercedes-benz-text">
+    </div>
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li class="nav-item"">
+                <a href="index.html" class="nav-link" id="home-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="vehicles.html" class="nav-link">Vehicles</a>
+            </li>
+            <li class="nav-item">
+                <a href="design.html" class="nav-link">Design</a>
+            </li>
+            <li class="nav-item">
+                <a href="history.html" class="nav-link">History</a>
+            </li>
+        </ul>
+    </nav>`;
+        header.insertAdjacentHTML('afterend', String.raw`<section class="empty-header-place"></section>`);
+    });
+
+    let footers = document.querySelectorAll(".footer");
+    footers.forEach(footer => footer.innerHTML = String.raw`<p>Copyright. 2020. ID: 00010837</p>`);
+})();
+
+
+
+
+
 
 //This piece of code will be executed if we are in main path, which is "/"
 // We need to put active class on home link
@@ -123,15 +133,16 @@ for (let i = 0; i < links.length; i++) {
     };
 
     // add event for 15 times per second
-    window.addEventListener('scroll', () => setTimeout(checkScroll, 1000/15));
+    window.addEventListener('scroll', () => setTimeout(checkScroll, 1000 / 15));
 })();
 
-// this function needed to play articles animations at correct time 
+// this function is needed to play articles animations at correct time 
 // when 200px or more of invisible first article are in viewport we add animate class and make all articles visible 
 // we make it one time animation by removing event listener at the end
 (function () {
-    let elements = document.querySelectorAll('.article');   
-
+    let elements = document.querySelectorAll('.article');
+    if (elements.length === 0)
+        return;
     document.addEventListener('scroll', animate);
 
     function shouldAnimate(el) {
@@ -151,4 +162,71 @@ for (let i = 0; i < links.length; i++) {
         }
     }
 
+})();
+
+(function () {
+    class VehicleGridItem {
+        constructor(name, price, description, imgUrl) {
+            this.name = name;
+            this.price = price;
+            this.description = description;
+            this.imgUrl = imgUrl;
+        }
+    }
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    buildHtmlForVehicle = function (vehicle) {
+        let vehicleHtml = String.raw`<div class="vehicle-container">`
+        vehicleHtml += String.raw`<p class="vehicle-name">` + vehicle.name + "</p>";
+        vehicleHtml += String.raw`<p class="vehicle-price">` + "Starting at $" + numberWithCommas(vehicle.price) + "</p>";
+        vehicleHtml += String.raw`<p class="vehicle-short-description">` + vehicle.description + "</p>";
+        vehicleHtml += String.raw`<img src="` + vehicle.imgUrl + String.raw`" alt="` + vehicle.name + "\"></div>";
+        return vehicleHtml;
+    };
+
+    let vehicles = [
+        new VehicleGridItem(" A-Class Sedan", 33650, "An entirely new class of Mercedes-Benz that sets a new standard for small sedans.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/a/all-vehicles/2021-A220-SEDAN-AVP-DR.png`),
+        new VehicleGridItem(" GLA SUV", 36230, "A true premium compact SUV with dramatic styling", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/gla/all-vehicles/2021-GLA250-AMG-LINE-SUV-AVP-DR.png`),
+        new VehicleGridItem(" CLA Coupe", 37850, "An entirely new seductive style at an even more irresistible price.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/cla/all-vehicles/2021-CLA250-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" GLB SUV", 38050, "An ideal blend of space, capability, technology and comfort.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/glb/all-vehicles/2021-GLB250-SUV-AVP-DR.png`),
+        new VehicleGridItem(" C-Class Sedan", 41600, "A stylish performer that embodies sophistication and intelligence.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/c/sedan/all-vehicles/2021-C300-SEDAN-AVP-DR.png`),
+        new VehicleGridItem(" GLC SUV", 43200, "It's more than a new name. It's the next generation of the SUV.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/glc/suv/all-vehicles/2021-GLC-SUV-AVP-DR.png`),
+        new VehicleGridItem(" C-Class Coupe", 47200, "Bold styling, sporty performance and competitive spirit in two doors.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/c/coupe/all-vehicles/2021-C300-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" SLC Roadster", 49950, "A compact two-seater with retractable hardtop for driving enthusiasts.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/slc/all-vehicles/2020-SLC300-AMG-LINE-ROADSTER-AVP-DR.png`),
+        new VehicleGridItem(" GLC Coupe", 51650, "Sporty coupe styling with SUV utility.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/glc/coupe/all-vehicles/2021-GLC300-4M-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" GLC SUV", 51900, "It's more than a new name. It's the next generation of the SUV.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/all-vehicles/2018-GLC-SUV-AV-LN.png`),
+        new VehicleGridItem(" E-Class Sedan", 54250, "Distinctive styling, class-leading safety and trailblazing technology.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/e/sedan/all-vehicles/2021-E350-SEDAN-AVP-DR.png`),
+        new VehicleGridItem(" C-Class Cabriolet", 54700, "A nimble and elegant soft-top convertible, for driving all year round.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/c/cab/all-vehicles/2021-C300-CABRIOLET-AVP-DR.png`),
+        new VehicleGridItem(" GLE SUV", 54750, "The next generation of safety and style in a luxury SUV.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/gle/suv/all-vehicles/2021-GLE-SUV-AVP-DR.png`),
+        new VehicleGridItem(" E-Class Coupe", 64950, "An alluring silhouette, with the performance and amenities to match.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/e/coupe/all-vehicles/2021-E-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" E-Class Wagon", 67600, "The quintessential luxury wagon has versatility and innovations to spare.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/e/wagon/all-vehicles/2021-E-WAGON-AVP-DR.png`),
+        new VehicleGridItem(" CLS Coupe", 70300, "A powerful expression of individuality in a visionary four-door coupe.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/cls/all-vehicles/2021-CLS450-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" E-Class Cabriolet", 71950, "All-season softtop convertible, with classic styling and comfort for four.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/e/cab/all-vehicles/2021-E450-CABRIOLET-AVP-DR.png`),
+        new VehicleGridItem(" GLS SUV", 76000, "Exceptional luxury and road manners, with seating for seven.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/gls/all-vehicles/2021-GLS450-SUV-AVP-DR.png`),
+        new VehicleGridItem(" GLE Coupe", 76500, "Pure sports coupe response, in a genuine SUV.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/gle/coupe/all-vehicles/2021-AMG-GLE53-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" SL Roadster", 91000, "Legendary two-seater engineered for enjoying open road and air alike.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/sl/all-vehicles/2020-SL450-ROADSTER-DR.png`),
+        new VehicleGridItem(" S-Class Sedan", 94250, "A leader in luxury, performance and safety - and of an entire industry.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/s/sedan/all-vehicles/2020-S450-SEDAN-AVP-DR.png`),
+        new VehicleGridItem(" Mercedes-AMG GT 4-door Coupe", 99950, "Four seats on the fast track.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/gt/4-door/all-vehicles/2020-AMG-GT53-4DR-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" Mercedes-AMG GT", 115900, "The highest level of luxury paired with an extreme level of performance.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/gt/coupe/all-vehicles/2020-AMG-GTC-AVP-DR.png`),
+        new VehicleGridItem(" Mercedes-AMG GT", 127900, "Open-air driving taken to a bold new level of performance.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/all-vehicles/2018-AMG-GT-C-ROADSTER-AV-LN.png`),
+        new VehicleGridItem(" S-Class Coupe", 130150, "Intricately intimate: A handcrafted haven for four.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/s/coupe/all-vehicles/2020-S560-AMGLINE-COUPE-AVP-DR.png`),
+        new VehicleGridItem(" G-Class SUV", 130900, "Extraordinary off-road ability meets handcrafted luxury and iconic style.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/g/all-vehicles/2020-G550-SUV-AVP-DR.png`),
+        new VehicleGridItem(" S-Class Cabriolet", 138600, "The convertible S-Class is reborn: A grand opening for a new day.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/s/cabriolet/all-vehicles/2020-S560-CABRIOLET-AVP-DR.png`),
+        new VehicleGridItem(" Mercedes-Maybach GLS SUV", 160500, "The new height of luxury.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my21/gls-maybach/all-vehicles/2021-MAYBACH-GLS-SUV-AVP-DR.png`),
+        new VehicleGridItem(" Mercedes-Maybach S-Class", 173000, "Like a private jet for the road, an experience beyond first class.", String.raw`https://www.mbusa.com/content/dam/mb-nafta/us/myco/my20/s/maybach/all-vehicles/2020-S560-MAYBACH-AVP-DR.png`)
+    ];
+
+
+    let vehiclesContainer = document.querySelector("#vehicles-container");
+    if (vehiclesContainer === null) {
+        return;
+    }
+
+    vehicles.forEach(vehicle => {
+        let vehicleHtml = buildHtmlForVehicle(vehicle);
+        vehiclesContainer.innerHTML += vehicleHtml;
+    });
 })();
